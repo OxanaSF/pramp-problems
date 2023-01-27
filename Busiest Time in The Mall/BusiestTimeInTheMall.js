5bd20c68bb55e0fbbea5ffc5ea7cbdbb4a50c5ff
OxanaSF <oxanatango@gmail.com>
2023-01-26 19:02:17 -0800

    add BusiestTimeInTheMall.js

---
+++ Busiest Time in The Mall/BusiestTimeInTheMall.js
@@ -0,0 +1,28 @@
+function findBusiestPeriod(data) {
+    let totalPeople = 0
+    let maxPeople = 0
+    let targetStamp = null
+   
+    
+    
+    for (let i = 0; i < data.length; i++) {
+  
+      if (data[i][2] === 1) {
+        totalPeople += data[i][1] 
+      } else {
+        if(data[i][2] === 0) totalPeople -= data[i][1]
+      }
+    
+      
+      if (i < data.length && data[i+1] && data[i][0] === data[i+1][0]) continue
+      
+      
+      if (totalPeople > maxPeople) {
+        maxPeople = totalPeople
+        targetStamp = data[i][0]     
+      }
+     
+    }
+    return targetStamp
+  }
+  
