aee33180a88315edb7a20033f5379eaf3fa6f85b
OxanaSF <oxanatango@gmail.com>
2023-02-03 20:06:59 -0800

    add description to arrayOfArrayProducts problem

--- Array of Array Products/ArrayOfArrayProducts.js
+++ Array of Array Products/ArrayOfArrayProducts.js
@@ -1,3 +1,13 @@
+// Array of Array Products
+// Given an array of integers arr, you’re asked to calculate 
+// for each index i the product of all integers except 
+// the integer at that index (i.e. except arr[i]). 
+// Implement a function arrayOfArrayProducts that takes 
+// an array of integers and returns an array of the products.
+
+// Solve without using division and analyze your solution’s 
+// time and space complexities.
+
 function arrayOfArrayProducts(arr) {
   let prevNumCollector = 1;
   let total = 1;
